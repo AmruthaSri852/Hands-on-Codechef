b1ddb00849a794b9809bd6061b4fb9cb42924974
A. Amrutha Sri <81810889+AmruthaSri852@users.noreply.github.com>
2021-11-30 12:08:37 +0530

    the one where it all began

---
+++ Codechef_contest1/01.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{ // your code goes here //c++
+	int t;
+	cin >> t;
+	
+	while (t--)
+	{   int n;
+	    cin>>n;
+    	if (n>=21)
+    	{
+	         cout<<"YES"<<endl;
+    	}
+    	else 
+    	{
+	         cout <<"NO"<<endl;
+    	}
+	}
+	return 0;
+
+}
