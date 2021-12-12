3e50481072b2910d60def95ffec3152f54c6d3ef
A. Amrutha Sri <81810889+AmruthaSri852@users.noreply.github.com>
2021-12-12 23:11:35 +0530

    Indivisible Permutation

---
+++ Starters19/INDIPERM.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,i,j;
+	    cin>>n;
+	        cout<<n<<" ";
+    	for (i=1;i<=n-1;i++){
+	            cout << i<< " ";
+    	}
+        	cout<<endl;
+	}
+ return 0;
+    
+}
+
+
